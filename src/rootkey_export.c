10d8adfc3df0c3b61f751e6333aa08c6f2d9edb5
jiaxv <zoujiaxv7891@gmail.com>
2025-06-04 11:01:22 +0800

    6.4

--- src/rootkey_export.c
+++ src/rootkey_export.c
@@ -54,32 +54,174 @@ char *get_table_name(ldacs_roles role) {
 #ifdef UNUSE_CRYCARD
 #define HOME_DIR "/home/jiaxv/"
 #else
-
 #define HOME_DIR "/root/"
-
 #endif
+
+
+const size_t as_count = 2;
+static const char *default_ASs[] = {
+    "000010010",
+    "000045678"
+};
+
+bool is_SGW = FALSE;
+
+int8_t generate_rkey();
+
+int8_t write_rkey_tocard(void);
+
+static void usage(const char *executable) {
+    printf("Usage: %s [-c config path] "
+           "[-H (http mode)] [-M (Merge mode)]\n",
+           executable);
+}
+
+
+int opt_parse(int argc, char *const *argv) {
+    int c;
+    while ((c = getopt(argc, argv, "as")) != -1) {
+        switch (c) {
+            case 'a':
+                is_SGW = FALSE;
+                write_rkey_tocard();
+                break;
+            case 's':
+                is_SGW = TRUE;
+                generate_rkey();
+                write_rkey_tocard();
+                break;
+            // case 't': {
+            //     config.timeout = strtol(optarg, NULL, 10);
+            //     break;
+            // }
+            // case 'w': {
+            //     config.worker = strtol(optarg, NULL, 10);
+            //     if (config.worker > sysconf(_SC_NPROCESSORS_ONLN)) {
+            //         fprintf(stderr,
+            //                 "Config ERROR: worker num greater than cpu available cores.\n");
+            //         return ERROR;
+            //     }
+            //     break;
+            // }
+            // case 'c': {
+            //     memcpy(config.config_path, optarg, strlen(optarg));
+            //     if (init_config_path() != OK) {
+            //         return ERROR;
+            //     }
+            //     parse_config(&config, config.config_path);
+            //     break;
+            // }
+            // case 'A':
+            // case 'G':
+            // case 'W': {
+            //     if (config.role != LD_UNDEFINED) {
+            //         return ERROR;
+            //     }
+            //     switch (c) {
+            //         case 'A':
+            //             config.role = LD_AS;
+            //             break;
+            //         case 'G':
+            //             config.role = LD_GS;
+            //             break;
+            //         case 'W':
+            //             config.role = LD_SGW;
+            //             break;
+            //         default:
+            //             return ERROR;
+            //     }
+            //     if (init_config_path() != OK) {
+            //         return ERROR;
+            //     }
+            //     parse_config(&config, config.config_path);
+            // }
+            // case 'H': {
+            //     config.use_http = TRUE;
+            //     break;
+            // }
+            // case 'M': {
+            //
+            //     if (config.role == LD_SGW) {
+            //         config.port = strtol("55551", NULL, 10);
+            //     }
+            //     config.is_merged = TRUE;
+            //     break;
+            // }
+            default: {
+                return ERROR;
+            }
+        }
+    }
+    return OK;
+}
 
 
 int main(int argc, char **argv) {
 #ifdef UNUSE_CRYCARD
     generate_kek(1);
 #endif
+    if (argc < 2 || opt_parse(argc, argv) != OK) {
+        usage(argv[0]);
+        exit(ERROR);
+    }
+
+    // #ifdef UNUSE_CRYCARD
+    //     generate_kek(1);
+    // #endif
+    //     char *db_name = get_db_name(LD_SGW);
+    //     char *table_name = get_table_name(LD_SGW);
+    //     if (km_rkey_gen_export("000010010", "000010000", ROOT_KEY_LEN, DEFAULT_VALIDATE, db_name, table_name,
+    //                            HOME_DIR".ldcauc/keystore/000010010_rootkey.bin")) {
+    //         log_error("根密钥生成、保存和导出失败。");
+    //     }
+    //     if (km_rkey_gen_export("000045678", "000010000", ROOT_KEY_LEN, DEFAULT_VALIDATE, db_name, table_name,
+    //                            HOME_DIR".ldcauc/keystore/000045678_rootkey.bin")) {
+    //         log_error("根密钥生成、保存和导出失败。");
+    //     }
+}
+
+
+int8_t generate_rkey() {
     char *db_name = get_db_name(LD_SGW);
     char *table_name = get_table_name(LD_SGW);
-    if (km_rkey_gen_export("000010010", "000010000", ROOT_KEY_LEN, DEFAULT_VALIDATE, db_name, table_name,
-                           HOME_DIR".ldcauc/keystore/000010010_rootkey.bin")) {
-        log_error("根密钥生成、保存和导出失败。");
+
+    for (int i = 0; i < as_count; i++) {
+        char *buf_dir = calloc(PATH_MAX, sizeof(char));
+        snprintf(buf_dir, PATH_MAX, HOME_DIR".ldcauc/keystore/%s_rootkey.bin", default_ASs[i]);
+        if (km_rkey_gen_export(default_ASs[i], "000010000", ROOT_KEY_LEN, DEFAULT_VALIDATE, db_name, table_name,
+                               buf_dir)) {
+            log_error("根密钥生成、保存和导出失败。 %s", default_ASs[i]);
+            free(db_name);
+            free(buf_dir);
+            return -1;
+        }
+        free(buf_dir);
+        log_info("Generate Rootkey for %s has Succeed", default_ASs[i]);
     }
-    if (km_rkey_gen_export("000045678", "000010000", ROOT_KEY_LEN, DEFAULT_VALIDATE, db_name, table_name,
-                           HOME_DIR".ldcauc/keystore/000045678_rootkey.bin")) {
-        log_error("根密钥生成、保存和导出失败。");
+    free(db_name);
+    return 0;
+}
+
+int8_t write_rkey_tocard(void) {
+    char *db_name = get_db_name(is_SGW ? LD_SGW : LD_AS);
+
+    for (int i = 0; i < as_count; i++) {
+        char *dir_buf = calloc(PATH_MAX, sizeof(char));
+        snprintf(dir_buf, PATH_MAX, HOME_DIR".ldcauc/keystore/%s_rootkey.bin", default_ASs[i]);
+        char *file_buf = calloc(PATH_MAX, sizeof(char));
+        snprintf(file_buf, PATH_MAX, "%s_rootkey.bin", default_ASs[i]);
+        if (km_writefile_to_cryptocard(dir_buf, file_buf) !=
+            LD_KM_OK) {
+            log_error("Error writing to ccard. %s", default_ASs[i]);
+            free(dir_buf);
+            free(file_buf);
+            free(db_name);
+            return -1;
+        }
+        free(dir_buf);
+        free(file_buf);
+        log_info("Install Rootkey for %s Has Succeed", default_ASs[i]);
     }
-    if (km_writefile_to_cryptocard(HOME_DIR".ldcauc/keystore/000010010_rootkey.bin", "000010010_rootkey.bin") !=
-        LD_KM_OK) {
-        log_error("Error writing to ccard.");
-    }
-    if (km_writefile_to_cryptocard(HOME_DIR".ldcauc/keystore/000045678_rootkey.bin", "000045678_rootkey.bin") !=
-        LD_KM_OK) {
-        log_error("Error writing to ccard.");
-    }
+    free(db_name);
+    return 0;
 }
